6cdba6ffa9d811ec23f97db2936f251754bcd25c
陈尚伟 <chenshangwei@geostar.com.cn>
2023-05-31 13:32:50 +0800

    chore: 测试是否成功引入CesiumJS

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import cesium from 'vite-plugin-cesium'; // 引入插件
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), cesium()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
 })
