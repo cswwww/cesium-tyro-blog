e0f7904cbd337122b77a235fbdf77018757b3254
ReBeX <420659880@qq.com>
2023-06-08 17:13:17 +0800

    build: 引入ElementPlus和mitt

--- src/main.js
+++ src/main.js
@@ -2,4 +2,15 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// 引入ElementPlue
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
+const app = createApp(App)
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) { // 引入ElementPlus Icon
+  app.component(key, component)
+}
+app.use(ElementPlus)
+app.mount('#app')
