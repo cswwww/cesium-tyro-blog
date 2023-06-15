0b0b2f3edb582096e70d6e2fd638178840c2ebb9
ReBeX <420659880@qq.com>
2023-06-15 09:03:26 +0800

    build: 引入vite-plugin-eslint

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,13 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import cesium from 'vite-plugin-cesium'; // 引入插件
 import path from 'path'
+import { defineConfig } from 'vite'
+import eslint from 'vite-plugin-eslint'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), cesium()],
+  plugins: [vue(), cesium(), eslint()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
