2ad4fd97076eb3d238ddc5f37b96f7b1e321126f
ReBeX <420659880@qq.com>
2023-07-28 18:37:34 +0800

    feat: 新增开场效果

--- src/utils/ImageryLayer/setImagery.js
+++ src/utils/ImageryLayer/setImagery.js
@@ -1,10 +1,13 @@
 /*
  * @Date: 2023-06-04 13:48:34
  * @LastEditors: ReBeX  420659880@qq.com
- * @LastEditTime: 2023-06-04 15:25:20
+ * @LastEditTime: 2023-07-28 16:57:00
  * @FilePath: \cesium-tyro-blog\src\utils\ImageryLayer\setImagery.js
  * @Description: 影像图层控制
  */
+
+import { viewer } from '@/utils/createCesium.js' // 引入地图对象
+import * as Cesium from 'cesium'
 
 // viewer.imageryLayers.get(0) // 获取序列号为0的图层
 // viewer.imageryLayers.indexOf(layer) // 图层的索引（层级，大的在上层）
@@ -14,3 +17,19 @@
 // viewer.imageryLayers.raiseToTop(layer); // 移到最上层
 // viewer.imageryLayers.remove(layer, true); // 移除图层
 // viewer.imageryLayers.removeAll(true); // 移除全部图层
+
+function showAllImagery(boolean = true) {
+  // 获取图像图层集合
+  const imageryLayers = viewer.imageryLayers;
+
+  // 遍历图像图层并隐藏它们
+  let numLayers = imageryLayers.length;
+  for (let i = 0; i < numLayers; i++) {
+    const layer = imageryLayers.get(i); // 获取图像图层对象
+    layer.show = boolean; // 设置图像图层隐藏
+  }
+}
+
+export {
+  showAllImagery
+}
---
+++ src/utils/Visualization/roaming.js
@@ -0,0 +1,65 @@
+/*
+ * @Date: 2023-07-28 16:21:53
+ * @LastEditors: ReBeX  420659880@qq.com
+ * @LastEditTime: 2023-07-28 18:18:34
+ * @FilePath: \cesium-tyro-blog\src\utils\Visualization\roaming.js
+ * @Description: 相机漫游效果
+ */
+
+import { viewer } from '@/utils/createCesium.js' // 引入地图对象
+import * as Cesium from 'cesium'
+import { showAllImagery } from '@/utils/ImageryLayer/setImagery.js'
+import darkEarth from '@/assets/images/darkEarth.jpg'
+
+function roaming() {
+  let isRoaming = true; // 漫游标志位
+  const DEFAULT_LIGHTING = viewer.scene.globe.enableLighting; // 默认光照状态
+  const DEFAULT_SKY_ATMOSPHERE = viewer.scene.skyAtmosphere.show; // 默认光照状态
+  let bgImglayer;
+
+  showAllImagery(false); // 隐藏所有图层
+  viewer.clock.multiplier = -2000.0;  // 时间加速！
+
+  bgImglayer = viewer.imageryLayers.addImageryProvider( // 加载背景底图
+    new Cesium.SingleTileImageryProvider({
+      url: '/src/assets/images/darkEarth.jpg'
+    })
+  );
+
+  if (!DEFAULT_LIGHTING) {
+    viewer.scene.globe.enableLighting = true; // 开启光照
+  }
+  if (!DEFAULT_SKY_ATMOSPHERE) {
+    viewer.scene.skyAtmosphere.show = true; // 开启天空大气，能在一定程度上降低地球轮廓锯齿
+  }
+
+  // 添加鼠标事件，触发后停止漫游效果
+  const handler = new Cesium.ScreenSpaceEventHandler(viewer.scene.canvas); // 交互句柄
+  handler.setInputAction((event) => {
+    isRoaming = false // 停止旋转
+    showAllImagery(true) // 显示图层
+    if (!DEFAULT_LIGHTING) {
+      viewer.scene.globe.enableLighting = false; // 关闭光照
+    }
+    if (!DEFAULT_SKY_ATMOSPHERE) {
+      viewer.scene.skyAtmosphere.show = false; // 关闭光照
+    }
+    viewer.imageryLayers.remove(bgImglayer, true); // 移除图层
+    viewer.clock.multiplier = 1;  // 正常时间流速
+
+    handler.removeInputAction(Cesium.ScreenSpaceEventType.LEFT_CLICK); // 移除鼠标事件监听
+  }, Cesium.ScreenSpaceEventType.LEFT_CLICK);
+
+  (function roamingEvent() {
+    if (isRoaming) {
+      // 控制相机围绕 Z 轴旋转
+      viewer.camera.rotate(Cesium.Cartesian3.UNIT_Z, Cesium.Math.toRadians(0.1));
+      requestAnimationFrame(roamingEvent);
+    }
+  })()
+}
+
+
+export {
+  roaming
+}
