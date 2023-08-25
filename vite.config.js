2548b5a2d682768b2c2b37c97165ae90153d9da3
ReBeX <420659880@qq.com>
2023-08-25 11:47:01 +0800

    chore: 修改eslint

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import cesium from 'vite-plugin-cesium'; // 引入插件
+import cesium from 'vite-plugin-cesium' // 引入插件
 import path from 'path'
-import eslint from 'vite-plugin-eslint'
-
+import eslintPlugin from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), cesium(), eslint()],
+  plugins: [vue(), cesium(), eslintPlugin({
+    include: ['src/**/*.ts', 'src/**/*.vue', 'src/*.ts', 'src/*.vue']
+  })],
+  base: '/v2/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
@@ -21,8 +23,8 @@ export default defineConfig({
         // modifyVars: {
         //   hack: `true; @import (reference) "${path.resolve("src/assets/css/base.less")}";`,
         // },
-        javascriptEnabled: true,
-      },
-    },
-  },
+        javascriptEnabled: true
+      }
+    }
+  }
 })
