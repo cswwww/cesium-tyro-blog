42540fefe9ca54fb8cbd858cabb79e3ad61fe4d2
ReBeX <420659880@qq.com>
2023-09-08 18:31:04 +0800

    feat: 新增关闭场景通用按钮

--- src/layouts/ToolBar.vue
+++ src/layouts/ToolBar.vue
@@ -1,7 +1,7 @@
 <!--
  * @Date: 2023-06-06 18:31:24
  * @LastEditors: ReBeX  420659880@qq.com
- * @LastEditTime: 2023-09-08 17:01:33
+ * @LastEditTime: 2023-09-08 18:30:35
  * @FilePath: \cesium-tyro-blog\src\layouts\ToolBar.vue
  * @Description: 工具栏界面组件（用来放功能按钮的
 -->
@@ -57,6 +57,19 @@ function showIM(component) {
         </el-row>
       </template>
     </el-popover>
+
+    <Teleport to="body">
+      <div class="closeButton">
+        <el-button v-if="sceneFlag" circle @click="sceneFlag = ''"  style="background-color: #ACB4C0;border: none;">
+          <svg t="1694168560335" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"
+            p-id="13686" width="20" height="20">
+            <path
+              d="M557.248 512l339.424 339.424-45.248 45.248L512 557.248 172.576 896.672 127.36 851.424 466.752 512 127.328 172.576 172.576 127.36 512 466.752 851.424 127.328l45.248 45.248z"
+              fill="#5D6D7E" p-id="13687"></path>
+          </svg>
+        </el-button>
+      </div>
+    </Teleport>
   </div>
 </template>
 
@@ -65,6 +78,24 @@ function showIM(component) {
   position: fixed;
   right: 16px;
   top: 16px;
+  display: flex;
+  flex-direction: row;
+
+  :deep(.el-button) {
+    margin-left: 10px;
+    height: auto;
+  }
+
+  img {
+    width: 20px;
+    height: 20px;
+  }
+}
+
+.closeButton {
+  position: fixed;
+  right: 16px;
+  top: 66px;
   display: flex;
   flex-direction: row;
 
